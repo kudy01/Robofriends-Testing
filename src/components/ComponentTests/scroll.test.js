9a2a6df64e841dc37d6a6175afa629518c385a63
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-11-12 21:28:32 +0530

    circle ci tests

--- src/components/ComponentTests/scroll.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { create } from "react-test-renderer";
-import scroll from '../scroll';
-
-
-it('Expect to render Scroll Component', () => {
-	expect(create(<scroll />).toJSON()).toMatchSnapshot()
-})
-
