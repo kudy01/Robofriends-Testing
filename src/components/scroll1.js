f31b0f10fb470ee5afaebefff29c9478f868c8ef
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-11-13 20:35:04 +0530

    Prettifying the code

---
+++ src/components/scroll1.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "5px solid black",
+        height: "800px",
+      }}
+    >
+      {
+        props.children /*all the children of app.js as we are wrapping the scroll */
+      }
+    </div>
+  );
+};
+
+export default Scroll;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll1 from "../components/scroll1";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "./App.css";
 
 const App = () => {
-	
-	const [robots, setRobots] = useState([]);
-	const [searchfield, setSearchfield] = useState('');
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-	useEffect(()=> {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response =>response.json())
-        .then(users => {setRobots(users)});
-	}, [])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
 
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value)
-	}
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
 
-	const filteredRobots = robots.filter(robot =>{
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-	
-	})
-	
-	return !robots.length ?  // when the page takes time to load as all the names come from an api
-	<h1>Loading</h1>:
-	(
-		<div className='tc'>
-			<h1 className='f1'>RoboFriends</h1>
-			<SearchBox searchChange={onSearchChange} />
-			<Scroll>
-				<ErrorBoundary>
-					<CardList robots={filteredRobots}/>
-				</ErrorBoundary>
-			</Scroll>
-		</div>	
-		);
-	}
-		
-	
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  return !robots.length ? ( // when the page takes time to load as all the names come from an api
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll1>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll1>
+    </div>
+  );
+};
+
 export default App;
